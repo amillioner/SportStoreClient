991fd0d51b47c11c128a24a9757db54963cd5a9f
anwar akhror <anwar.akhror@outlook.com>
2020-03-10 19:31:36 -0400

    web service all operations

--- src/app/models/repository.ts
+++ src/app/models/repository.ts
@@ -1,6 +1,7 @@
 import { Product } from './product.model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Filter } from './configClasses.repository';
 
 const productsUrl = "/api/products";
 
@@ -9,8 +10,11 @@ export class Reporsitory {
 
     product: Product;
     products: Product[];
+    filter: Filter = new Filter();
 
     constructor(private http: HttpClient) {
+        this.filter.category = "soccer";
+        this.filter.related = true;
         this.getProducts();
     }
     // constructor() {
@@ -26,8 +30,18 @@ export class Reporsitory {
         this.http.get<Product>(`${productsUrl}/${id}`)
             .subscribe(p => this.product = p);
     }
-    getProducts(related = false) {
-        this.http.get<Product[]>(`${productsUrl}?related=${related}`)
-            .subscribe(prods => this.products = prods);
+    getProducts() {
+        let url = `${productsUrl}?related=${this.filter.related}`;
+        if (this.filter.category) {
+            url += `&category=${this.filter.category}`;
+        }
+        if (this.filter.search) {
+            url += `&search=${this.filter.search}`;
+        }
+        this.http.get<Product[]>(url).subscribe(prods => this.products = prods);
     }
+    // getProducts(related = false) {
+    //     this.http.get<Product[]>(`${productsUrl}?related=${related}`)
+    //         .subscribe(prods => this.products = prods);
+    // }
 }
