bf291f344548a31eb73477aafaebed3170f0296f
anwar akhror <anwar.akhror@outlook.com>
2020-03-16 23:49:12 -0400

    submitting order

--- src/app/models/model.module.ts
+++ src/app/models/model.module.ts
@@ -3,12 +3,13 @@ import { Repository } from './repository';
 import { HttpClientModule } from "@angular/common/http";
 import { NavigationService } from './navigation.service';
 import { Cart } from './cart.model';
+import { Order } from './order.model';
 
 // The next type of module is the Angular feature module, which is used to group related functionality together, such as the data repository classes in the data model. Using feature modules makes Angular applications easier to manage and maintain, although the real benefits are available when working with Angular building blocks such as components
 // This is the decorator:
 @NgModule({
     imports: [HttpClientModule],
-    providers: [Repository, NavigationService, Cart]
+    providers: [Repository, NavigationService, Cart, Order]
 })
 
 export class ModelModule { }
--- src/app/store.module.ts
+++ src/app/store.module.ts
@@ -9,11 +9,16 @@ import { RatingsComponent } from './store/ratings.component';
 import { CartDetailComponent } from './store/cartDetail.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CheckoutDetailsComponent } from './store/checkout/checkoutDetails.component';
+import { CheckoutPaymentComponent } from './store/checkout/checkoutPayment.component';
+import { CheckoutSummaryComponent } from './store/checkout/checkoutSummary.component';
+import { OrderConfirmationComponent } from './store/checkout/orderConfirmation.component';
 
 @NgModule({
   declarations: [CartSummaryComponent, CategoryFilterComponent,
     PaginationComponent, ProductListComponent, RatingsComponent,
-    ProductSelectionComponent, CartDetailComponent],
+    ProductSelectionComponent, CartDetailComponent,
+    CheckoutDetailsComponent, CheckoutPaymentComponent, CheckoutSummaryComponent, OrderConfirmationComponent],
   imports: [BrowserModule, FormsModule, RouterModule],
   exports: [ProductSelectionComponent]
 })
